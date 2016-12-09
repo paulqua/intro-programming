1e3bd283956743db47adefaa20c603b9ce165988
Paul Qua <paulwqua@gmail.com>
2016-12-08 20:21:48 -0600

    Change directions to form a flower

--- python/mindstorms.py
+++ python/mindstorms.py
@@ -2,8 +2,13 @@ import turtle
 
 def draw_square(some_turtle):
 	for i in range(1, 5):
-		some_turtle.forward(100)
-		some_turtle.right(90)
+		some_turtle.forward(70)
+		some_turtle.right(45)
+		some_turtle.forward(50)
+		some_turtle.right(135)
+		some_turtle.forward(50)
+		some_turtle.right(45)
+		some_turtle.forward(70)
 
 def draw_art():
 	window = turtle.Screen()
@@ -12,10 +17,12 @@ def draw_art():
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("green")
-	brad.speed(2)
+	brad.speed(4)
 	for i in range(1, 37):
 		draw_square(brad)
 		brad.right(10)
+	brad.right(90)
+	brad.forward(200)
 	##Create the turtle angie- Draw a Circle
 	#angie = turtle.Turtle()
 	#angie.shape("arrow")
